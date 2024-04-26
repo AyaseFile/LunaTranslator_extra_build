5844f7ddb991d032d6a3e9f286feee5168567027
恍兮惚兮 <101191390+HIllya51@users.noreply.github.com>
2024-04-26 09:41:24 +0800

    Update build.py

--- build.py
+++ build.py
@@ -261,5 +261,5 @@ if __name__ == "__main__":
 
     subprocess.run(f"curl -LO https://github.com/tcnksm/ghr/releases/download/v0.16.2/ghr_v0.16.2_windows_amd64.zip")
     subprocess.run(f"7z x ghr_v0.16.2_windows_amd64.zip")
-    subprocess.run(f'ghr_v0.16.2_windows_amd64/ghr -u HIllya51 -r LunaTranslator_extra_build -recreate common ALL.zip')
+    subprocess.run(f'ghr_v0.16.2_windows_amd64/ghr -u HIllya51 -r LunaTranslator_extra_build -replace common ALL.zip')
           
